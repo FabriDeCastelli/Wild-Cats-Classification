585df9429896e9dad59f39f1c60b33d2ca443f08
fabriziodecastelli <decastellifabrizio@gmail.com>
2023-05-15 11:56:47 +0200

    Configuration changes

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import save_model, load_model
 
+
 def load_imgs(path, folders):
     imgs = []
     labels = []
@@ -22,18 +23,19 @@ def load_imgs(path, folders):
     print('Loaded {} images total.'.format(n_imgs))
     return imgs, labels
 
+
 def plot_sample(imgs, labels, nrows=4, ncols=4, resize=None):
     # create a grid of images
-    fig, axs = plt.subplots(nrows, ncols, figsize=(4*ncols, 4*nrows))
+    fig, axs = plt.subplots(nrows, ncols, figsize=(4 * ncols, 4 * nrows))
     # take a random sample of images
-    indices = np.random.choice(len(imgs), size=nrows*ncols, replace=False)
+    indices = np.random.choice(len(imgs), size=nrows * ncols, replace=False)
     for ax, idx in zip(axs.reshape(-1), indices):
         ax.axis('off')
         # sample an image
         ax.set_title(labels[idx])
         im = imgs[idx]
         if isinstance(im, np.ndarray):
-            im = Image.fromarray(im)  
+            im = Image.fromarray(im)
         if resize is not None:
             im = im.resize(resize)
         ax.imshow(im, cmap='gray')
@@ -41,24 +43,25 @@ def plot_sample(imgs, labels, nrows=4, ncols=4, resize=None):
 
 # map class -> idx
 label_to_idx = {
-    'CHEETAH':0,
+    'CHEETAH': 0,
     'OCELOT': 1,
-    'SNOW LEOPARD':2, 
-    'CARACAL':3,
+    'SNOW LEOPARD': 2,
+    'CARACAL': 3,
     'LIONS': 4,
     'PUMA': 5,
-    'TIGER':6
+    'TIGER': 6
 }
 
 idx_to_label = {
-    0:'CHEETAH',
-    1:'OCELOT',
-    2:'SNOW LEOPARD', 
-    3:'CARACAL',
-    4:'LIONS',
-    5:'PUMA',
-    6:'TIGER'
+    0: 'CHEETAH',
+    1: 'OCELOT',
+    2: 'SNOW LEOPARD',
+    3: 'CARACAL',
+    4: 'LIONS',
+    5: 'PUMA',
+    6: 'TIGER'
 }
+
 
 def make_dataset(imgs, labels, label_map, img_size):
     x = []
@@ -68,16 +71,14 @@ def make_dataset(imgs, labels, label_map, img_size):
         # preprocess img
         x_i = im.resize(img_size)
         x_i = np.asarray(x_i)
-        
+
         # encode label
         y_i = np.zeros(n_classes)
         y_i[label_map[l]] = 1.
-        
+
         x.append(x_i)
         y.append(y_i)
     return np.array(x).astype('float32'), np.array(y)
-
-
 
 
 def save_keras_model(model, filename):
